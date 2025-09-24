706f9f5badf3d2aa04fd5c09e98f23302b677eef
shravankumar89 <shravanknitnexus@gmail.com>
2025-09-25 02:37:39 +0530

    frontend issues

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -17,8 +17,7 @@ async  function createToken(email: string) {
 }
 
 function setAuthCookie(c: Context, token: string) {
-    setCookie(c, COOKIE_NAME, token, { httpOnly: true, path: "/", maxAge: COOKIE_MAX_AGE, sameSite: "Strict",   secure: process.env.NODE_ENV === "production" ? true : false
-
+    setCookie(c, COOKIE_NAME, token, { httpOnly: true, path: "/", maxAge: COOKIE_MAX_AGE, sameSite: "None", secure: true
     });
 }
 
